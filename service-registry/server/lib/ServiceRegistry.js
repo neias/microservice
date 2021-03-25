a46e844cfeb5475e20c350f600bccacf143084c4
emre acar <emreacar@hotmail.com>
2021-03-25 16:54:08 +0300

    null

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -1,25 +1,27 @@
 class ServiceRegistry {
-  constructor(log){
+
+  constructor(log) {
     this.log = log;
-    this.service = {};
+    this.services = {};
     this.timeout = 30;
   }
 
   register(name, version, ip, port) {
     const key = name + version + ip + port;
 
-    if (!this.serices[key]) {
+
+    if (!this.services[key]) {
       this.services[key] = {};
       this.services[key].timestamp = Math.floor(new Date() / 1000);
       this.services[key].ip = ip;
       this.services[key].port = port;
       this.services[key].name = name;
       this.services[key].version = version;
-      this.log.debug(`Added service ${name}, version ${version}, at ${ip}:${port}`);
+      this.log.debug(`Added services ${name}, version ${version} at ${ip}:${port}`);
       return key;
     }
     this.services[key].timestamp = Math.floor(new Date() / 1000);
-    this.log.debug(`Updated service ${name}, version ${version}, at ${ip}:${port}`);
+    this.log.debug(`Updated services ${name}, version ${version} at ${ip}:${port}`);
     return key;
   }
 }
