42f758a195b490af16c0862c91d8801db0af9c66
emre acar <emreacar@hotmail.com>
2021-03-26 17:35:12 +0300

    null

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -1,3 +1,5 @@
+const semver = require('semver');
+
 class ServiceRegistry {
 
   constructor(log) {
@@ -6,7 +8,16 @@ class ServiceRegistry {
     this.timeout = 30;
   }
 
+  get(name, version) {
+    this.cleanup();
+    const candidates = Object.values(this.services)
+          .filter(service => service.name === name && semver.satisfies(service.version, version));
+
+    return candidates[Math.floor(Math.random() * candidates.length)];
+  }
+
   register(name, version, ip, port) {
+    this.cleanup();
     const key = name + version + ip + port;
 
 
@@ -23,6 +34,22 @@ class ServiceRegistry {
     this.services[key].timestamp = Math.floor(new Date() / 1000);
     this.log.debug(`Updated services ${name}, version ${version} at ${ip}:${port}`);
     return key;
+  }
+
+  unregister(name, version, ip, port){
+    const key = name + version + ip + port;
+    delete this.services[key];
+    return key;
+  }
+
+  cleanup(){
+    const now = Math.floor(new Date() / 1000);
+    Object.keys(this.services).forEach((key) => {
+      if(this.services[key].timestamp + this.timeout < now){
+        delete this.services[key];
+        this.log.debug(`Removed services ${key}`);
+      }
+    });
   }
 }
 
--- service-registry/server/service.js
+++ service-registry/server/service.js
@@ -24,12 +24,42 @@ module.exports = (config) => {
     return res.json({ result: serviceKey });
   });
 
-  service.delete('/register/:servicename/:serviceversion/:serviceport', (req, res, next) => {
-    return next("not implemented");
+  service.delete('/register/:servicename/:serviceversion/:serviceport', (req, res) => {
+    const { servicename, serviceversion, serviceport } = req.params;
+
+    const serviceip = req.connection.remoteAddress.includes('::') ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+
+    const serviceKey = serviceRegistry.unregister(servicename, serviceversion, serviceip, serviceport);
+    return res.json({ result: serviceKey });
   });
 
-  service.get('/find/:servicename/:serviceversion', (req, res, next) => {
-    return next("not implemented");
+  service.get('/find/:servicename/:serviceversion', (req, res) => {
+    const {servicename, serviceversion } = req.params;
+    const svc = serviceRegistry.get(servicename, serviceversion);
+
+    if(!svc) return res.status(404).json({ result: 'Service not found' });
+
+    return res.json(svc);
+  });
+
+  service.get('/list', (req, res, next) => {
+    return next('not implemented');
+  });
+
+  service.get('/list-short', (req, res, next) => {
+    return next('not implemented');
+  });
+
+  service.get('/names', (req, res, next) => {
+    return next('not implemented');
+  });
+
+  service.get('/speaker/:shortname', (req, res, next) => {
+    return next('not implemented');
+  });
+
+  service.get('/artwork/:shortname', (req, res, next) => {
+    return next('not implemented');
   });
 
   // eslint-disable-next-line no-unused-vars
